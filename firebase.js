ad2288ad9369abdec77a27a1da6f4df81ab829e8
Jordan M. Gomes <139926932+jmgomes03@users.noreply.github.com>
2024-08-04 23:13:26 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from 'firebase/firestore';
+import { getFirestore} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBN3kTM5lauy97FPBtsCJNvLr6btmGpbx0",
-  authDomain: "inventory-management-f6039.firebaseapp.com",
-  projectId: "inventory-management-f6039",
-  storageBucket: "inventory-management-f6039.appspot.com",
-  messagingSenderId: "413048593818",
-  appId: "1:413048593818:web:c509f73a9f18376123ae23",
-  measurementId: "G-TQ0P8Y8WJS"
+  apiKey: "AIzaSyBjYRfKsGS_KEL2T8m4aMAkkA8siYNM2Xo",
+  authDomain: "inventory-management-30bd4.firebaseapp.com",
+  projectId: "inventory-management-30bd4",
+  storageBucket: "inventory-management-30bd4.appspot.com",
+  messagingSenderId: "878041097354",
+  appId: "1:878041097354:web:bc2d2f4440ff20502a7973",
+  measurementId: "G-6JENDWKSDX"
 };
 
 // Initialize Firebase
