980f17110c1cae1d288ef3b41d4ffda98eff427b
jmgomes03 <jordangomes360@gmail.com>
2024-08-04 22:27:40 -0400

    first commit

---
+++ app/page.js
@@ -0,0 +1,230 @@
+'use client'
+import { useState, useEffect } from 'react';
+import { firestore } from '@/firebase';
+import { Box, Modal, Typography, Stack, TextField, Button } from '@mui/material';
+import { collection, deleteDoc, doc, getDocs, query, getDoc, setDoc } from 'firebase/firestore'; // Ensure query is imported
+import Image from 'next/image'; // Import Image
+
+export default function Home() {
+  // variables
+  const [inventory, setInventory] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [itemName, setItemName] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [showHelp, setShowHelp] = useState(false); // State for Help section visibility
+
+  // fetching function
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'));
+    const docs = await getDocs(snapshot);
+    const inventoryList = [];
+    docs.forEach((doc) => {
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      });
+    });
+    setInventory(inventoryList);
+    console.log(inventoryList);
+  };
+
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      await setDoc(docRef, { quantity: quantity + 1 });
+    } else {
+      await setDoc(docRef, { quantity: 1 });
+    }
+    await updateInventory();
+  };
+
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      if (quantity === 1) {
+        await deleteDoc(docRef);
+      } else {
+        await setDoc(docRef, { quantity: quantity - 1 });
+      }
+    }
+    await updateInventory();
+  };
+
+  useEffect(() => {
+    updateInventory();
+  }, []);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const toggleHelp = () => setShowHelp((prev) => !prev); // Toggle help visibility
+
+  return (
+    <Box width="100vw" height="100vh" 
+      display="flex" 
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      gap={2}
+      sx={{
+        backgroundImage: 'url(/market.jpg)', 
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      {/* Fruit basket icon */}
+      <Image src="/fruit-basket.png" alt="Fruit Basket" width={100} height={100} />
+
+      {/* Title */}
+      <Typography variant="h1" color="#c0bbf9" style={{ marginBottom: 70 }}>
+        Track Your Inventory
+      </Typography>
+
+      <Modal open={open} onClose={handleClose}>
+        <Box 
+          position="absolute"
+          top="50%"
+          left="50%"
+          width={400}
+          bgcolor="white"
+          border="2px solid #000"
+          boxShadow={24}
+          p={4}
+          display="flex"
+          flexDirection="column"
+          gap={3} 
+          sx={{ transform: 'translate(-50%,-50%)' }}
+        >
+          <Typography variant="h6">Add Item</Typography>
+          <Stack width="100%" direction="row" spacing={2}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              value={itemName}
+              onChange={(e) => {
+                setItemName(e.target.value);
+              }}
+            />
+            <Button
+              variant="outlined"
+              onClick={() => {
+                addItem(itemName);
+                setItemName('');
+                handleClose();
+              }}
+            >
+              Add
+            </Button>
+          </Stack>
+        </Box>
+      </Modal>
+
+      <Button variant="contained" onClick={handleOpen}>
+        Add New Item
+      </Button>
+
+      <TextField
+        variant="outlined"
+        placeholder="Search for items"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        style={{ marginBottom: '20px', width: '800px' }}
+      />
+
+      <Box border="3px solid #333">
+        <Box
+          width="800px"
+          height="100px"
+          bgcolor="#ADD7E5"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Typography variant="h2" color="black">
+            Inventory Items
+          </Typography>
+        </Box>
+        
+        <Stack width="800px" height="300px" spacing={2} overflow="auto">
+          {inventory
+            .filter(item => item.name.toLowerCase().includes(searchQuery.toLowerCase()))
+            .map(({ name, quantity }) => (
+              <Box 
+                key={name} 
+                width="100%" 
+                minHeight="150px" 
+                display="flex"
+                alignItems="center" 
+                justifyContent="space-between"
+                bgcolor="white"
+                padding={5}
+              >
+                <Typography
+                  variant="h3"
+                  color="black"
+                  textAlign="center"
+                >
+                  {name.charAt(0).toUpperCase() + name.slice(1)}  
+                </Typography>
+                <Typography
+                  variant="h3"
+                  color="black"
+                  textAlign="center"
+                >
+                  {quantity}
+                </Typography>
+                <Stack direction="row" spacing={2}>
+                  <Button 
+                    variant="contained" 
+                    onClick={() => {
+                      addItem(name);
+                    }}  
+                  > 
+                    Add an Item 
+                  </Button>
+
+                  <Button 
+                    variant="contained" 
+                    onClick={() => {
+                      removeItem(name);
+                    }}  
+                    sx={{ background: 'red', color: 'white' }}
+                  > 
+                    Remove an Item 
+                  </Button>
+                </Stack>
+              </Box>
+            ))
+          }
+        </Stack>
+      </Box>
+
+      {/* help button */}
+      <Button variant="outlined" onClick={toggleHelp} sx={{ mt: 2 }}>
+        Help
+      </Button>
+
+      {/* help section */}
+      {showHelp && (
+        <Box width="600px" bgcolor="#ffe5b9" p={2} borderRadius={2} mt={4}>
+          <Typography variant="h6" color="black" textAlign="center">
+            Help
+          </Typography>
+          <Typography variant="h6" color="black" mt={1}>
+            Add new item: creates a new item for your inventory.
+            <br />
+            Add an item: adds one more quantity of the respective item.
+            <br />
+            Remove an item: deletes one item of the respective item.
+          </Typography>
+        </Box>
+      )}
+    </Box>
+  );
+}
---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore} from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBjYRfKsGS_KEL2T8m4aMAkkA8siYNM2Xo",
+  authDomain: "inventory-management-30bd4.firebaseapp.com",
+  projectId: "inventory-management-30bd4",
+  storageBucket: "inventory-management-30bd4.appspot.com",
+  messagingSenderId: "878041097354",
+  appId: "1:878041097354:web:bc2d2f4440ff20502a7973",
+  measurementId: "G-6JENDWKSDX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app)
+
+export {firestore}
